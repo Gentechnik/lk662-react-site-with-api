596ff2a031ca647da1e29f9ffe1d8a6fedc6de6b
Lukas Karberg <deaderlp@googlemail.com>
2024-01-03 11:38:11 +0100

    Add axios dependency and fetch app name from backend

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export const PageWelcome = () => {
+	const backendUrl = "http://localhost:4882";
+	const [appMessage, setAppMessage] = useState("");
+	const [appName, setAppName] = useState("");
+
+	const fetchAppName = async () => {
+		try {
+			const response = await axios.get(backendUrl);
+			setAppName(response.data.appName);
+		} catch (error: any) {
+			console.log(`ERROR: ${error.message}`);
+			setAppMessage(
+				`Sorry, we can't retrieve your data at this time. Try again later.`
+			);
+		}
+	};
+
+	useEffect(() => {
+		fetchAppName();
+	}, []);
+
 	return (
-		<p>This is the welcome page.</p>
-	)
-}
+		<>
+			<h2>{appMessage}</h2>
+			{appMessage === "" && (
+				<>
+					<p>APPNAME: {appName}</p>
+				</>
+			)}
+		</>
+	);
+};
